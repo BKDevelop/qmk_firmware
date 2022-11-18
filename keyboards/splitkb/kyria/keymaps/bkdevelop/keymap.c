31e2b14c1f60818251848358bc53f86ad6217a0a
Benjamin Köhler <benjaminkoehler89@gmail.com>
2022-11-18 16:31:58 +0100

    tryout mod-tap in gaming layer

--- keyboards/splitkb/kyria/keymaps/bkdevelop/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bkdevelop/keymap.c
@@ -73,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAMING] = LAYOUT(
-    KC_T, KC_TAB , KC_Q, KC_W  , KC_E   , KC_R,                                        KC_Y   , KC_U   , KC_I,    KC_O,   KC_P   , KC_BSPC, 
-    KC_G, KC_LSFT, KC_A, KC_S  , KC_D   , KC_F,                                        KC_BSLS, KC_J   , KC_K,    KC_L,   KC_SCLN, KC_DEL, 
-    KC_B, KC_LCTL, KC_Z, KC_X  , KC_C   , KC_V, KC_H  , KC_CAPS,     FKEYS  , KC_LGUI, KC_RBRC, KC_QUOT, KC_COMM, KC_DOT, KC_SLSH, KC_RALT, 
-                         KC_ESC, KC_LALT, KC_N, KC_SPC, KC_M,        SYM  , KC_ENT , KC_LBRC, NAV  , ADJUST
+    KC_T, KC_TAB , KC_Q, KC_W            , KC_E   , KC_R,                                        KC_Y   , KC_U   , KC_I,    KC_O,   KC_P   , KC_BSPC, 
+    KC_G, KC_LSFT, KC_A, KC_S            , KC_D   , KC_F,                                        KC_BSLS, KC_J   , KC_K,    KC_L,   KC_SCLN, KC_DEL, 
+    KC_B, KC_LCTL, KC_Z, KC_X            , KC_C   , KC_V, KC_H  , KC_CAPS,     FKEYS  , KC_LGUI, KC_RBRC, KC_QUOT, KC_COMM, KC_DOT, KC_SLSH, KC_RALT, 
+                         LT(_SYM, KC_ESC), KC_LALT, KC_N, KC_SPC, KC_M,        SYM  , KC_ENT , KC_LBRC, NAV  , ADJUST
     ),
 
 /*
