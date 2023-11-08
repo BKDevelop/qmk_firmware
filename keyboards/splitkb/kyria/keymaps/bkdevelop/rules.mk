b20631945e40748696ef4c6df1d9105f9a1f901d
Benjamin Köhler <benjaminkoehler89@gmail.com>
2023-11-08 11:54:58 +0100

    convert to liatris

--- keyboards/splitkb/kyria/keymaps/bkdevelop/rules.mk
+++ keyboards/splitkb/kyria/keymaps/bkdevelop/rules.mk
@@ -2,3 +2,6 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 UNICODEMAP_ENABLE = yes
+# Convert from Elite-C to Liatris
+CONVERT_TO=liatris
+
