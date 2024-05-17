6d100267c065db69f523b4af2059badb814bfd5c
Benjamin Köhler <benjaminkoehler89@gmail.com>
2024-05-17 23:58:09 +0200

    changes after pulling upstream

--- keyboards/splitkb/kyria/keymaps/bkdevelop/rules.mk
+++ keyboards/splitkb/kyria/keymaps/bkdevelop/rules.mk
@@ -1,7 +1,7 @@
 OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 UNICODEMAP_ENABLE = yes
 # Convert from Elite-C to Liatris
 CONVERT_TO=liatris
+EXTRAKEY_ENABLE = yes      # Audio control and System control
 
