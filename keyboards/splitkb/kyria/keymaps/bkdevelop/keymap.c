da7c3ad519f2aa81e0fccc045dbea38158eea3a2
Benjamin Köhler <benjaminkoehler89@gmail.com>
2025-07-17 18:54:08 +0200

    adjust SYM layer switch on gaming layer

--- keyboards/splitkb/kyria/keymaps/bkdevelop/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bkdevelop/keymap.c
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GAMING] = LAYOUT(
     KC_TAB, KC_G,  KC_Q, KC_W , KC_E   , KC_R,                                        KC_Y   , KC_U   , KC_I,    KC_O,   KC_P   , KC_BSPC,
     KC_T, KC_LSFT, KC_A, KC_S , KC_D   , KC_F,                                        KC_BSLS, KC_J   , KC_K,    KC_L,   KC_SCLN, KC_DEL,
-    KC_B, KC_LCTL, KC_Z, KC_X , KC_C   , KC_V, KC_H, LT(_SYM, KC_CAPS),     FKEYS  , KC_LGUI, KC_RBRC, KC_QUOT, KC_COMM, KC_DOT, KC_SLSH, KC_RALT,
-                         LT(_FUNCTION, KC_ESC), KC_LALT, KC_N, KC_SPC, KC_M,        SYM  , KC_ENT , KC_LBRC, NAV  , QWERTY
+    KC_B, KC_LCTL, KC_Z, KC_X , KC_C   , KC_V, KC_H, SYM,     FKEYS  , KC_LGUI, KC_RBRC, KC_QUOT, KC_COMM, KC_DOT, KC_SLSH, KC_RALT,
+                         LT(_FUNCTION, KC_ESC), KC_LALT, KC_N, KC_SPC, KC_M,        KC_CAPS , KC_ENT , KC_LBRC, NAV  , QWERTY
     ),
 
     [_COLEMAK_DH] = LAYOUT(
